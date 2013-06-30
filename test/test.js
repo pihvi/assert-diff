0170f562a865fd1203b3e1b8cdd12bcc38e3f4f2
Antti Mattila <antti.mattila@reaktor.fi>
2013-06-30 17:25:49 +0300

    Fix array handling i.e. passing arguments to original assert

--- test/test.js
+++ test/test.js
@@ -1,6 +1,22 @@
 var assert = require('./../lib/assert-diff')
 var assertOrig = require('assert')
 
+it('diff deep equal array', function() {
+  assert.deepEqual([1, "loi", {pow: true}], [1, "lol", {pow: true}])
+})
+
+it('original deep equal array', function() {
+  assertOrig.deepEqual([1, "loi", {pow: true}], [1, "lol", {pow: true}])
+})
+
+it('diff deep equal string', function() {
+  assert.deepEqual("tomato", "tomeito")
+})
+
+it('original deep equal string', function() {
+  assertOrig.deepEqual("tomato", "tomeito")
+})
+
 it('diff deep equal', function() {
   assert.deepEqual({pow: "boom", foo: 2}, {bar: 2, pow: "bang"})
 })
