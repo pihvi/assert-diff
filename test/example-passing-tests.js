85a53045f0d5d7ce89261b144be4c93efab2f957
fent <933490+fent@users.noreply.github.com>
2020-03-04 17:28:15 -0500

    Fix async assert functions

--- test/example-passing-tests.js
+++ test/example-passing-tests.js
@@ -38,3 +38,27 @@ it('basic derived asserts works', function() {
   assert.equal(3, 3, 'this should not fail')
   assert.object({}, 'this should not fail');
 })
+
+it('async functions', async function() {
+  let a = false;
+  await assert.doesNotReject(function() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        a = true;
+        resolve();
+      })
+    })
+  })
+  assert.ok(a);
+
+  let b = false;
+  await assert.rejects(async function() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        b = true;
+        reject(Error('bad error'))
+      })
+    })
+  });
+  assert.ok(b);
+})
